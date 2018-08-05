82a4314a3f8f65d198bad6d88e20955b0f9a9659
Niv Sardi <xaiki@evilgiggle.com>
2018-08-04 21:58:15 -0300

    we're rendering from preact, no need to hydrate
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { hydrate, render } from 'react-dom'
+import { render } from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import './global.css'
 import Index from './pages'
 import registerServiceWorker from './registerServiceWorker'
 
 const root = document.getElementById('root')
-if (root.hasChildNodes()) {
-    hydrate(<Router><Index /></Router>, root)
-} else {
-    render(<Router><Index /></Router>, root)
-}
+render(<Router><Index /></Router>, root)
 registerServiceWorker()
