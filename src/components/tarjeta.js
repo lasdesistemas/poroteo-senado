2dfe79763c0d58fb83858bc6d532d77a00f9fc38
Niv Sardi <xaiki@evilgiggle.com>
2018-08-07 10:46:52 -0300

    forgot <li>
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/tarjeta.js
+++ src/components/tarjeta.js
@@ -14,7 +14,7 @@ export default ({titulo, votos, color, senators}) => {
         <div className='tarjeta-header' style={{ color: colors[color]}}>
             <h3><span>{titulo}</span></h3>
             <ul>
-                {senators.map(s => s.Senador.split(',')[0])}
+                {senators.map(s => <li>{s.Senador.split(',')[0]}</li>)}
             </ul>
         </div>
         <div className={`tarjeta-body ${color}`}
