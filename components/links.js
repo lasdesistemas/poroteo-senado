ef915d0d0e34772290e42a98617baacac9db7f27
Nayla Portas <naylaportas@gmail.com>
2020-11-28 18:06:30 -0300

    Modifica links

--- components/links.js
+++ components/links.js
@@ -1,35 +1,29 @@
 
 export default () => (
 
-    <div className='links'>
-      <p> Más info en:</p>
-      <a href='https://activaelcongreso.org/'>
-        <img
-          className='activa-logo'
-          src='static/assets/img/logos/activa2.png'
-          alt='Activa el Congreso' />
-      </a>
-      <a href='https://docs.google.com/spreadsheets/d/1TUTag7Majqhn5noRLLMUJ6SFJ0Phwlo-Oc1T59uSZCE/edit#gid=219162967'>
-        <img
-          className='ecofeminita-logo'
-          src='static/assets/img/logos/ecofeminita.png'
-          alt='Ecofeminita' />
-      </a>
+  <div className='links'>
+    <p> Datos de:</p>
+    <a href='https://docs.google.com/spreadsheets/d/1TUTag7Majqhn5noRLLMUJ6SFJ0Phwlo-Oc1T59uSZCE/edit#gid=219162967'>
+      <img
+        className='ecofeminita-logo'
+        src='static/assets/img/logos/ecofeminita.png'
+        alt='Ecofeminita' />
+    </a>
 
-     <style jsx>{`
-        .activa-logo {
-            height: 80px;
-            margin: 5px;
+   <style jsx>{`
+      .activa-logo {
+          height: 80px;
+          margin: 5px;
+      }
+      .ecofeminita-logo {
+          height: 90px;
+          margin: 5px;
+      }
+      .links {
+          display: flex;
+          justify-content: center;
+          align-self: flex-end;
         }
-        .ecofeminita-logo {
-            height: 90px;
-            margin: 5px;
-        }
-        .links {
-            display: flex;
-            justify-content: center;
-            align-self: flex-end;
-          }
-      `}</style>
-    </div>
-  )
+    `}</style>
+  </div>
+)
