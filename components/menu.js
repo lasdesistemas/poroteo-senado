3df7d09abcb9e2150ce2b319259fc13ad33ecdd3
Nayla Portas <naylaportas@gmail.com>
2020-11-28 18:00:39 -0300

    Agrega menu

---
+++ components/menu.js
@@ -0,0 +1,10 @@
+export default () => (
+    <div className='menu'>
+        <span className='item'><a href='https://poroteodiputades.herokuapp.com/'>
+        Ver diputades
+        </a></span>
+        <span className='item'><a href='https://ecofeminita.github.io/poroteoaborto2020/#gr%C3%A1ficos'>
+        Más info
+        </a></span>
+    </div>
+  )
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from '../components/head'
 import Header from '../components/header'
 import Tarjeta from '../components/tarjeta'
+import Menu from '../components/menu'
 import FechaActualizacion from '../components/fecha-actualizacion'
 import Footer from '../components/footer'
 import axios from 'axios'
@@ -54,6 +55,7 @@ export default class extends React.Component {
       <div className='container'>
             <Head/>
             <Header />
+            <Menu/>
             {
               this.state.votos &&
               <div className="fila">
